4d0c0d56dcfa9088f6d57e5c34696d57b046821d
Iván Rodríguez <ivan.rodriguez@soltelsevilla.onmicrosoft.com>
2024-03-26 17:22:38 +0100

    [+] Creado formulario para endpoint Insertar Cliente

--- src/app/models/clientes.model.ts
+++ src/app/models/clientes.model.ts
@@ -1,2 +1,8 @@
-export class Clientes {
+// Spring Boot 3.2 + Angular v17: Paso 2 -> Modelo (interfaz)
+// la Diferencia con una clase es que la interfaz modela un JSON
+export interface Clientes {
+    nif: string
+    nombre: string
+    edad: number
+    sexo: number
 }
