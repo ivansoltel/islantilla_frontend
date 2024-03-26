c2b4974bfdf129728dcccb2a0225a852fbb1534f
Iván Rodríguez <ivan.rodriguez@soltelsevilla.onmicrosoft.com>
2024-03-26 16:48:40 +0100

    [+] Commit Inicial Proyecto Islantilla Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormulariosClientesComponent } from './formularios-clientes/formularios-clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulariosClientesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/formularios-clientes/formularios-clientes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formularios-clientes',
+  templateUrl: './formularios-clientes.component.html',
+  styleUrl: './formularios-clientes.component.css'
+})
+export class FormulariosClientesComponent {
+
+}
